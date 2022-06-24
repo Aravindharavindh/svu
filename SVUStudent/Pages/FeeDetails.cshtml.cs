01f6155ae4e0be41a7b0ae25309931f3e9983bf5
IyyappanAvia <iyyappan@avia.co.in>
2022-06-24 13:54:25 +0530

    Design Update

---
+++ SVUStudent/Pages/FeeDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SVUStudent.Pages
+{
+    public class FeeDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SVUStudent/Reports/TermFeeReport.cs
@@ -0,0 +1,18 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace SVUStudent.Reports
+{
+    public partial class TermFeeReport
+    {
+        public TermFeeReport()
+        {
+            InitializeComponent();
+        }
+
+        private System.Void label1_BeforePrint(System.Object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+
+        }
+    }
+}
